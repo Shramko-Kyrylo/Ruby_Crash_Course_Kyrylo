e4b9accbdc0d03375035b55f687c697c721cb751
Shramcko-Kyrylo <Shramckokirill@gmail.com>
2023-02-26 16:50:03 +0200

    H/w #2

--- urok2.rb
+++ urok2.rb
@@ -13,7 +13,9 @@ arr = ["apple", true, "kiwi", 4, 88, "33", false, undefined, "JS"]
 newArr = []
 
 arr.each { |i| newArr << i if i == i.to_s }
+
 p newArr
+puts 
 
 
 
@@ -25,5 +27,43 @@ arr.each { |i| x[i] += 1 }
 moda = x.max_by { |k, v| v }[0]
 
 new_arr = arr.select{ |i| i != moda }
+
 p moda
 p new_arr
+puts 
+
+
+
+#4) Знайти в масиві двозначні(10+), трьохзначні(100+) та чотирьохзначні(1000+)  числа та повертає в масив їх кількість .
+arr = [1,2,55,3,100, 333, 9999];
+
+xx = arr.count { |i| i.between?(10, 99) }
+xxx = arr.count { |i| i.between?(100, 999) }
+xxxx = arr.count { |i| i.between?(1000, 9999) }
+
+p res = [xx, xxx, xxxx]
+puts 
+
+
+#5) Є масив [5, “Limit”, 12, “a”, “3”, 99, 2, [2, 4, 3, “33”, “a”, “text”], “strong”, “broun”]
+#Вивести нові масиви яка складаються із даних початкового масиву, але одного типу даних (не приводити тип стрінг в число навіть якщо там лише число)
+arr = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun']
+arr_1 = arr.flatten
+
+int = arr_1.select { |i| i == i.to_i }
+str = arr_1.select { |i| i == i.to_s }
+
+p res = [int, str]
+puts 
+
+
+
+#6) Дано рядок із іменами, перетворити їх в масив, в масиві повидаляти лишні пробели, посортувати значення і вивести в новий масив імена які більше 4 символів мають, а в старий масив вивести відсортовані значення і об'єднати їх в рядок
+names = "Alex,    Orysia, Misha,    Ira     , Paul";
+
+arr = names.split(',').map(&:strip)
+sort_arr = arr.sort
+big_names = arr.select { |i| i.length > 4 }
+
+p sort_arr
+p big_names
